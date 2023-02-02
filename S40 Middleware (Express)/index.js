5c2db7372511fd0800ded65c2168e8c91ce31fe9
Sherkhan K <sherkhan4work@gmail.com>
2023-02-02 11:40:07 +0600

    Middleware, example on 'morgan'

---
+++ S40 Middleware (Express)/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan')
+
+app.use(morgan('tiny'))
+
+app.get('/', (req, res) => {
+    res.send('HOME PAGE')
+})
+app.get('/dogs', (req, res) => {
+    res.send('WOOF WOOF')
+})
+
+app.listen(3000, () => {
+    console.log('App is running on localhost:3000');
+})
